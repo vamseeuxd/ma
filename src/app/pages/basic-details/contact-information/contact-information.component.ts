2c71972207089ce01ebfe9fd4bac75ea1b6e579b
vamseeuxd <vamsee.uxd@gmail.com>
2018-10-05 11:01:47 +0530

    Done with contact information funtionality

--- src/app/pages/basic-details/contact-information/contact-information.component.ts
+++ src/app/pages/basic-details/contact-information/contact-information.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {AngularFireDatabase, AngularFireObject} from '@angular/fire/database';
 
 @Component({
   selector: 'ngx-contact-information',
@@ -6,17 +7,34 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./contact-information.component.scss'],
 })
 export class ContactInformationComponent implements OnInit {
-
+  showBusyIndicator = false;
   data = {};
+  private contactInformationTable: AngularFireObject<any>;
 
-  constructor() {
+  constructor(private db: AngularFireDatabase) {
+    this.contactInformationTable = this.db.object('contactInformation');
+    this.contactInformationTable.valueChanges().subscribe(success => {
+      if (success) {
+        this.data = success;
+        setTimeout(() => {
+          this.showBusyIndicator = false;
+        }, 2000);
+      } else {
+        this.showBusyIndicator = false;
+      }
+    }, error => {
+      this.showBusyIndicator = false;
+    });
   }
 
   ngOnInit() {
   }
 
-  onSubmitButtonClick(basicInformationForm: any, fax) {
-    console.log(fax.invalid);
+  onSubmitButtonClick() {
+    this.showBusyIndicator = true;
+    this.contactInformationTable.set(this.data).then(result => {
+      this.showBusyIndicator = false;
+    });
   }
 
 }
