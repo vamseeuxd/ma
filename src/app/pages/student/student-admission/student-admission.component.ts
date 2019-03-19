a78d8fc481a2306349c77733f2af9f91a194ff62
vamseeuxd <vamsee.uxd@gmail.com>
2019-03-19 22:04:50 +0530

    Worked on Firebase integration for student Admission Page

--- src/app/pages/student/student-admission/student-admission.component.ts
+++ src/app/pages/student/student-admission/student-admission.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AngularFireDatabase, AngularFireObject} from '@angular/fire/database';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
 
 @Component({
   selector: 'ngx-student-admission',
@@ -11,15 +13,15 @@ export class StudentAdmissionComponent implements OnInit {
 
   data = {};
   private studentAdmissionTable: AngularFireObject<any>;
-  optionsSeason: Array<any>;
-  optionsGender: Array<any>;
-  optionsClass: Array<any>;
-  optionsSection: Array<any>;
+  optionsSeason: Observable<any[]> = new Observable<any[]>();
+  optionsGender: Observable<any[]> = new Observable<any[]>();
+  optionsClass: Observable<any[]> = new Observable<any[]>();
+  optionsSection: Observable<any[]> = new Observable<any[]>();
+
 
   constructor(private db: AngularFireDatabase) {
     this.showBusyIndicator = true;
     this.studentAdmissionTable = this.db.object('studentAdmission');
-
     this.studentAdmissionTable.valueChanges().subscribe(success => {
       if (success) {
         this.data = success;
@@ -32,32 +34,59 @@ export class StudentAdmissionComponent implements OnInit {
     }, error => {
       this.showBusyIndicator = false;
     });
+    this.getSessions();
+  }
+
+  getSessions() {
+    this.optionsSeason = this.db.list('sessionsList').snapshotChanges().pipe(
+      map(changes =>
+        changes.map(c => ({key: c.payload.key, ...c.payload.val()})),
+      ),
+    );
+
+    this.optionsGender = this.db.list('gender').snapshotChanges().pipe(
+      map(changes =>
+        changes.map(c => ({key: c.payload.key, ...c.payload.val()})),
+      ),
+    );
+
+    this.optionsClass = this.db.list('sessionsList').snapshotChanges().pipe(
+      map(changes =>
+        changes.map(c => ({key: c.payload.key, ...c.payload.val()})),
+      ),
+    );
+
+    this.optionsSection = this.db.list('sessionsList').snapshotChanges().pipe(
+      map(changes =>
+        changes.map(c => ({key: c.payload.key, ...c.payload.val()})),
+      ),
+    );
   }
 
   ngOnInit() {
-    this.optionsSeason = [
-      { id: '1', label: '2018' },
-      { id: '2', label: '2019' },
-      { id: '3', label: '2020' },
+    /*this.optionsSeason = [
+      {id: '1', label: '2018'},
+      {id: '2', label: '2019'},
+      {id: '3', label: '2020'},
     ];
 
     this.optionsGender = [
-      { id: '1', label: 'Male' },
-      { id: '2', label: 'Female' },
-      { id: '3', label: 'Trans Gender' },
+      {id: '1', label: 'Male'},
+      {id: '2', label: 'Female'},
+      {id: '3', label: 'Trans Gender'},
     ];
 
     this.optionsClass = [
-      { id: '1', label: 'Class 1' },
-      { id: '2', label: 'Class 2' },
-      { id: '3', label: 'Class 3' },
+      {id: '1', label: 'Class 1'},
+      {id: '2', label: 'Class 2'},
+      {id: '3', label: 'Class 3'},
     ];
 
     this.optionsSection = [
-      { id: '1', label: 'Section A' },
-      { id: '2', label: 'Section B' },
-      { id: '3', label: 'Section C' },
-    ];
+      {id: '1', label: 'Section A'},
+      {id: '2', label: 'Section B'},
+      {id: '3', label: 'Section C'},
+    ];*/
   }
 
   onSubmitButtonClick() {
